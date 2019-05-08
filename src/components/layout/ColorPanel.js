d7c57550b24ea056c1d2112d71bf61d785a354b5
JohanGJensen <johancentralen@gmail.com>
2019-05-08 22:27:08 +0100

    added instant text color update

--- src/components/layout/ColorPanel.js
+++ src/components/layout/ColorPanel.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 //style
 import injectSheet from "react-jss";
 import { colorPanel } from "../style/style.js";
@@ -8,25 +9,36 @@ import { changeColorText } from "../actions/actions";
 
 class ColorPanel extends Component {
   changeColor = e => {
-    const { classes, changeColorText, theme } = this.props;
+    const { classes, changeColorText, theme, memeText } = this.props;
     const color = e.target.className;
+
+    const vid = document.getElementById("meme");
+    const canvas = document.getElementById("videoCanvas");
+    const ctx = canvas.getContext("2d");
 
     switch (color) {
       case classes.colorWhite:
         changeColorText(theme.palette.text);
+        ctx.fillStyle = theme.palette.text;
         break;
       case classes.colorPrimary:
         changeColorText(theme.palette.primary);
+        ctx.fillStyle = theme.palette.primary;
         break;
       case classes.colorSecondary:
         changeColorText(theme.palette.secondary);
+        ctx.fillStyle = theme.palette.secondary;
         break;
       case classes.colorBlack:
         changeColorText(theme.palette.tertiary);
+        ctx.fillStyle = theme.palette.tertiary;
         break;
       default:
         changeColorText(theme.palette.text);
     }
+
+    ctx.drawImage(vid, 0, 0, 800, 450);
+    ctx.fillText(memeText, 400, 400, 600);
   };
 
   render() {
@@ -43,7 +55,15 @@ class ColorPanel extends Component {
   }
 }
 
+ColorPanel.propTypes = {
+  memeText: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+  memeText: state.memeState.memeText
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   { changeColorText }
 )(injectSheet(colorPanel)(ColorPanel));
