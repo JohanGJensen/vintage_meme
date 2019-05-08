5ca969860828b8538bd7067899b65a54debd6bd5
JohanGJensen <johancentralen@gmail.com>
2019-05-08 18:31:35 +0100

    Updates

---
+++ precache-manifest.bdc548c1455149e64f0c6699069138b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c915389222e73cf8a67",
+    "url": "/vintage_meme/static/css/main.1b4d1fa8.chunk.css"
+  },
+  {
+    "revision": "8c915389222e73cf8a67",
+    "url": "/vintage_meme/static/js/main.f70d1b45.chunk.js"
+  },
+  {
+    "revision": "7277603ba7729acad3da",
+    "url": "/vintage_meme/static/js/runtime~main.16c2906a.js"
+  },
+  {
+    "revision": "b3325373df2e72330b61",
+    "url": "/vintage_meme/static/js/2.e3c59395.chunk.js"
+  },
+  {
+    "revision": "87c3edb47bd543f133aba14af88e9752",
+    "url": "/vintage_meme/static/media/Righteous-Regular.87c3edb4.ttf"
+  },
+  {
+    "revision": "37ffcc334bd4ff1872e732944afdb425",
+    "url": "/vintage_meme/index.html"
+  }
+];
