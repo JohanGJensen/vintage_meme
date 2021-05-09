0f6e042c2eb29e7f6fe46f5317f5e0a8d16c160f
JohanGJensen <johancentralen@gmail.com>
2021-05-09 10:51:42 +0200

    fix(reducer): remove un important reducer actions

--- src/state/reducers/reducers.js
+++ src/state/reducers/reducers.js
@@ -1,26 +1,11 @@
 import * as actionType from "../actions/types";
 
 const initialState = {
-  titleColor: "primary",
-  memeText: "",
-  memeGallery: [],
-  duration: "white",
+  duration: "0",
 };
 
 export function videoState(state = initialState, action) {
   switch (action.type) {
-    case actionType.TOGGLE_TITLE_COLOR:
-      return {
-        ...state,
-        titleColor: state.titleColor === "primary" ? "secondary" : "primary",
-      };
-    case actionType.WRITE_MEME_CAPTION:
-      return { ...state, memeText: action.payload };
-    case actionType.SAVE_MEME:
-      return {
-        ...state,
-        memeGallery: [...state.memeGallery, action.payload],
-      };
     case actionType.CHANGE_DURATION:
       return { ...state, duration: action.payload };
 
